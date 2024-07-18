ffefc0c5ef6e281033fce0a05e200c17326311e5
Jamart Peres <alt.damon.j@gmail.com>
2024-07-18 17:03:15 -0600

    cambios en instrucciones pasillo (estetica lista)

--- src/components/InstruccionesPasillo.jsx
+++ src/components/InstruccionesPasillo.jsx
@@ -1,22 +1,26 @@
-
 import videoSrc from '../assets/videos/Pasillo-instrucciones.mp4'; 
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './InstruccionesPasillo.module.css';
+import forwardArrow from '../assets/right-arrow.png'; // Importamos la imagen de la flecha
 
 const InstruccionesPasillo = ({ onClose }) => {
   return (
     <div className={styles.overlay}>
       <div className={styles.popup}>
-        <button className={styles.closeButton} onClick={onClose}>×</button>
         <h2>Recorre el pasillo deslizandolo y elige una sala dando clic en la puerta</h2>
         <div className={styles.videoContainer}>
-          <video className={styles.video} autoPlay loop muted>
+          <video className={`${styles.video} ${styles.exampleVideo}`} autoPlay loop muted>
             <source src={videoSrc} type="video/mp4" />
             Tu navegador no soporta el elemento de video.
           </video>
         </div>
-        <button className={styles.okButton} onClick={onClose}>OK</button>
+        <img 
+          src={forwardArrow} 
+          alt="Forward Arrow" 
+          className={styles.forwardArrow} 
+          onClick={onClose}
+        />
       </div>
     </div>
   );
